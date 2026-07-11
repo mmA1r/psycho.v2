fcbb149227525059d84a2ecf35edffdfda068e02
mmAir <aang.matveev@gmail.com>
2026-07-11 17:55:33 +0400

    feat: update icons to use new SVG format

--- components/icons/Percent/index.vue
+++ components/icons/Percent/index.vue
@@ -1,6 +1,5 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-        <title>Symbol-percent SVG Icon</title>
         <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
             <circle cx="18.5" cy="17" r="4.2"/>
             <circle cx="29.5" cy="30.1" r="4.2"/>
